f525e04c192eae6029492baa02ea6f9c18697408
Szymon Zych <szymonzych868@gmail.com>
2026-01-22 20:49:11 +0100

    feat: Comment out "Projekty" nav item in navItems for future reference

--- work/src/data/navItems.ts
+++ work/src/data/navItems.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: "Strona Główna", href: "/" },
   { name: "O mnie", href: "/#about" },
-  { name: "Projekty", href: "/projects" },
+  // { name: "Projekty", href: "/projects" },
   { name: "Proces", href: "/#process" },
   { name: "Cennik", href: "/#pricing" },
   { name: "Kontakt", href: "/contact" },
